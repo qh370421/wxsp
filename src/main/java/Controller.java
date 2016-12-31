e2a7c6a50f719fa9d6006b57199e9692d2df484b
qinhu <763385274@qq.com>
2016-12-31 12:57:45 +0800

    初始化

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 //使用自动配置，主动添加并解析bean，配置文件等信息
 @EnableAutoConfiguration
 
-public class Controller {
+public class Controller{
     //设置访问的url
     @RequestMapping("/")
     //表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
